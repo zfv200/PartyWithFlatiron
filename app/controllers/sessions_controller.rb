d73049dcff03fa6b53f17ff964655d83cdc4b794
github username <github email address>
2018-03-26 16:48:10 -0400

    added logout button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,4 +16,9 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.delete :user_id
+    redirect_to '/'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   post '/users', to: 'users#create'
   get '/home', to: 'welcome#home'
   post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
